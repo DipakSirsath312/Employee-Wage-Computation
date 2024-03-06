f4423a875f6c63e8e7ec153b882e4b879741f77d
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-03-06 12:20:40 +0530

    [MODIFIED]:Dipak - UC-1- Completed Check Employee Present And Absent

--- src/com/dipak/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/dipak/employeewagecomputation/EmployeeWageComputation.java
@@ -4,7 +4,19 @@ public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
 		
+		int attendance; 
+		Random random = new Random();
+		attendance = random.nextInt(9)%2;
+		
 		System.out.println("WelCome Employee wage Computation...!");
+		
+		if(attendance==1) {
+			System.out.println("Employee is Presente...!");
+			
+		}else {
+			System.out.println("Employee is Absent!");
+			
+		}
 		
 	}
 
