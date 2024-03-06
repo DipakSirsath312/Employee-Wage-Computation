42d200eb6f8484c82764b7dbff243cae1f8d8109
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-03-06 12:38:43 +0530

    [MODIFIED]:Dipak - Calculate Employee Daily Wage

--- src/com/dipak/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/dipak/employeewagecomputation/EmployeeWageComputation.java
@@ -3,18 +3,23 @@ import java.util.*;
 public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
-		
 		int attendance; 
+		final int WAGE_PER_HOUR=20;
+		final int FULL_DAY_HOUR=8;
+		int daily_emp_wage=0; 
+		
 		Random random = new Random();
 		attendance = random.nextInt(9)%2;
 		
 		System.out.println("WelCome Employee wage Computation...!");
 		
 		if(attendance==1) {
-			System.out.println("Employee is Presente...!");
+			System.out.println("Employee is Present...!");
+			daily_emp_wage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+			System.out.println("Employee Daily Wages :-"+daily_emp_wage);
 			
 		}else {
-			System.out.println("Employee is Absent!");
+			System.out.println("Employee is Absent...!");
 			
 		}
 		
