1d1b8767d755867b8f58134f6fb652d6d3054997
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-03-06 12:00:54 +0530

    [ADDED]:Dipak - Show the Welcome Message

---
+++ src/com/dipak/employeewagecomputation/EmployeeWageComputation.java
@@ -0,0 +1,11 @@
+package com.dipak.employeewagecomputation;
+import java.util.*;
+public class EmployeeWageComputation {
+
+	public static void main(String[] args) {
+		
+		System.out.println("WelCome Employee wage Computation...!");
+		
+	}
+
+}
