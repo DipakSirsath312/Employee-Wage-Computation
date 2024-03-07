3166be76118e6db0f570bee199cbb29ba0bc362a
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-03-07 20:51:29 +0530

    [MODIFIED]:Dipak - Employee Type Check Functionality

--- src/com/dipak/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/dipak/employeewagecomputation/EmployeeWageComputation.java
@@ -3,24 +3,31 @@ import java.util.*;
 public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
-		int attendance; 
+		int attendance=0; 
 		final int WAGE_PER_HOUR=20;
 		final int FULL_DAY_HOUR=8;
+		final int Part_Time_Hour=4;
+		int Emp_Type;
 		int daily_emp_wage=0; 
 		
 		Random random = new Random();
 		attendance = random.nextInt(9)%2;
-		
 		System.out.println("WelCome Employee wage Computation...!");
-		
-		if(attendance==1) {
-			System.out.println("Employee is Present...!");
-			daily_emp_wage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-			System.out.println("Employee Daily Wages :-"+daily_emp_wage);
-			
-		}else {
-			System.out.println("Employee is Absent...!");
-			
+		if(attendance==0) {
+			System.out.println("Employee Is Absent...!");
+		}
+		else {
+			System.out.println("Employee Is Present...!");
+			Emp_Type = random.nextInt(9)%2;
+			if(Emp_Type == 0) {
+				System.out.println("Employee Is Part Time");
+				daily_emp_wage =WAGE_PER_HOUR * Part_Time_Hour;
+			}
+			else {
+				System.out.println("Employee Is Full Time");
+				daily_emp_wage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+			}
+			System.out.println("Employee Daily Wages :"+daily_emp_wage);
 		}
 		
 	}
