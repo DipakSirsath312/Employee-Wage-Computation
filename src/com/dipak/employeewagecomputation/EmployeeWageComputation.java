05b5475359d13bef74a75fc35eae81a7574c2059
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-03-07 22:23:50 +0530

    [MODIFIED]:Dipak - Refactored Code From if-else To Switch-Case

--- src/com/dipak/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/dipak/employeewagecomputation/EmployeeWageComputation.java
@@ -3,7 +3,10 @@ import java.util.*;
 public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
-		int attendance=0; 
+		
+		System.out.println("WelCome Employee wage Computation...!");
+		
+		int attendance; 
 		final int WAGE_PER_HOUR=20;
 		final int FULL_DAY_HOUR=8;
 		final int Part_Time_Hour=4;
@@ -12,24 +15,26 @@ public class EmployeeWageComputation {
 		
 		Random random = new Random();
 		attendance = random.nextInt(9)%2;
-		System.out.println("WelCome Employee wage Computation...!");
-		if(attendance==0) {
-			System.out.println("Employee Is Absent...!");
+		switch(attendance) {
+			case 0:
+				System.out.println("Employee Is Absent");
+				break;
+			case 1:
+				System.out.println("Employee Is Present");
+				Emp_Type = random.nextInt(9)%2;
+				switch(Emp_Type) {
+				case 0:
+					System.out.println("Employee Is Part Time");
+					daily_emp_wage =WAGE_PER_HOUR * Part_Time_Hour;
+					break;
+				case 1:
+					System.out.println("Employee Is Full Time");
+					daily_emp_wage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+					break;
+				}
+				System.out.println("Employee Daily Wages :"+daily_emp_wage);
+				break;		
 		}
-		else {
-			System.out.println("Employee Is Present...!");
-			Emp_Type = random.nextInt(9)%2;
-			if(Emp_Type == 0) {
-				System.out.println("Employee Is Part Time");
-				daily_emp_wage =WAGE_PER_HOUR * Part_Time_Hour;
-			}
-			else {
-				System.out.println("Employee Is Full Time");
-				daily_emp_wage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-			}
-			System.out.println("Employee Daily Wages :"+daily_emp_wage);
-		}
-		
 	}
 
 }
