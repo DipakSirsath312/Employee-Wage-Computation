2609943b58a6446effd977e56b00be042426b66a
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-03-09 20:27:26 +0530

    [MODIFIED]:Dipak - Modified Code For Class And Object

--- src/com/dipak/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/dipak/employeewagecomputation/EmployeeWageComputation.java
@@ -1,35 +1,37 @@
 package com.dipak.employeewagecomputation;
 import java.util.*;
 public class EmployeeWageComputation {
-
-	public static void main(String[] args) {
-		
+	
+	int attendance; 
+	int Emp_Type;
+	int daily_emp_wage = 0;
+	int Monthly_Wage = 0;
+	int Total_work_hour = 0;
+	int Total_work_days = 1;
+	final int WAGE_PER_HOUR = 20;
+	final int FULL_DAY_HOUR = 8; 
+	final int Part_Time_Hour = 4;
+	final int Work_Days = 20;
+	
+	public void Employeewagecomp()
+	{
 		System.out.println("WelCome Employee wage Computation...!");
-		
-		int attendance; 
-		int Emp_Type;
-		int daily_emp_wage = 0;
-		int Monthly_Wage = 0;
-		int Total_work_hour = 0;
-		int Total_work_days = 1;
-		final int WAGE_PER_HOUR = 20;
-		final int FULL_DAY_HOUR = 8; 
-		final int Part_Time_Hour = 4;
-		final int Work_Days = 20;
-		
-			while(Total_work_hour <=100 && Total_work_days <=20){
+		while(Total_work_hour <=100 && Total_work_days <=20)
+		{
 			System.out.println("Day:"+Total_work_days);
 			Random random = new Random();
 			attendance = random.nextInt(9)%2;
 			
-			switch(attendance) {
+			switch(attendance) 
+			{
 				case 0:
 					System.out.println("Employee Is Absent");
 					break;
 				case 1:
 					System.out.println("Employee Is Present");
 					Emp_Type = random.nextInt(9)%2;
-					switch(Emp_Type) {
+					switch(Emp_Type) 
+					{
 					case 0:
 						System.out.println("Employee Is Part Time");
 						daily_emp_wage = WAGE_PER_HOUR * Part_Time_Hour;
@@ -52,6 +54,11 @@ public class EmployeeWageComputation {
 					
 		}
 			System.out.println("Employee Monthly Wage:"+Monthly_Wage);
-	}
-
-}     
+}
+	
+public static void main(String[] args) {
+	
+	EmployeeWageComputation employeewage = new EmployeeWageComputation();
+	employeewage.Employeewagecomp();
+	}     
+}
