9dc4bdde233c8ccb963ca8332216f5210f87ae67
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-03-09 23:35:09 +0530

    [MODIFIED]:Dipak - completed for Find Multiple componies Empl… …oyee Wages using Class And Method

--- src/com/dipak/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/dipak/employeewagecomputation/EmployeeWageComputation.java
@@ -8,20 +8,23 @@ public class EmployeeWageComputation {
 	int Monthly_Wage = 0;
 	int Total_work_hour = 0;
 	int Total_work_days = 1;
-	final int WAGE_PER_HOUR = 20;
-	final int FULL_DAY_HOUR = 8; 
-	final int Part_Time_Hour = 4;
-	final int Work_Days = 20;
-	
-	public void Employeewagecomp()
+	int wage_per_hour ;
+	int full_day_hour= 8; 
+	int Part_Time_Hour = 4;
+	int Work_Days  ;
+	int monthly_working_hour ;
+
+	public void Employeewagecomp(int wage_per_hour , int monthly_working_hour ,int Work_Days)
 	{
+		this.wage_per_hour = wage_per_hour ;
+	    this.Work_Days = Work_Days;
+		this.monthly_working_hour = monthly_working_hour ;
 		System.out.println("WelCome Employee wage Computation...!");
-		while(Total_work_hour <=100 && Total_work_days <=20)
+		while(Total_work_hour <=  monthly_working_hour && Total_work_days <= Work_Days )
 		{
 			System.out.println("Day:"+Total_work_days);
 			Random random = new Random();
 			attendance = random.nextInt(9)%2;
-			
 			switch(attendance) 
 			{
 				case 0:
@@ -34,13 +37,13 @@ public class EmployeeWageComputation {
 					{
 					case 0:
 						System.out.println("Employee Is Part Time");
-						daily_emp_wage = WAGE_PER_HOUR * Part_Time_Hour;
+						daily_emp_wage = this.wage_per_hour * this.Part_Time_Hour;
 						Total_work_hour = Total_work_hour + Part_Time_Hour;
 						break;
 					case 1:
 						System.out.println("Employee Is Full Time");
-						daily_emp_wage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-						Total_work_hour = Total_work_hour + FULL_DAY_HOUR;
+						daily_emp_wage = this.wage_per_hour * this.full_day_hour;
+						Total_work_hour = Total_work_hour + this.full_day_hour;
 						break;
 					}
 					
@@ -50,15 +53,16 @@ public class EmployeeWageComputation {
 			}
 			System.out.println("Total Working Hour:"+Total_work_hour);
 			System.out.println("--------------------------");
-			Total_work_days++;
-					
+			Total_work_days++;		
 		}
 			System.out.println("Employee Monthly Wage:"+Monthly_Wage);
 }
-	
 public static void main(String[] args) {
 	
 	EmployeeWageComputation employeewage = new EmployeeWageComputation();
-	employeewage.Employeewagecomp();
+	employeewage.Employeewagecomp(20 ,100 , 20);
+	
+	EmployeeWageComputation employeewage1 = new EmployeeWageComputation();
+	employeewage1.Employeewagecomp(10 ,50 , 15);
 	}     
 }
