a267eacfa426f5fa5f512ae00a083391415c4692
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-03-09 13:22:57 +0530

    [MODIFIED]:Dipak - Calculated Total Working Hour And Days

--- src/com/dipak/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/dipak/employeewagecomputation/EmployeeWageComputation.java
@@ -10,13 +10,15 @@ public class EmployeeWageComputation {
 		int Emp_Type;
 		int daily_emp_wage = 0;
 		int Monthly_Wage = 0;
+		int Total_work_hour = 0;
+		int Total_work_days = 1;
 		final int WAGE_PER_HOUR = 20;
 		final int FULL_DAY_HOUR = 8; 
 		final int Part_Time_Hour = 4;
 		final int Work_Days = 20;
 		
-		for(int i = 1;i <=Work_Days;i++) {
-			System.out.println("Day:"+i);
+			while(Total_work_hour <=100 && Total_work_days <=20){
+			System.out.println("Day:"+Total_work_days);
 			Random random = new Random();
 			attendance = random.nextInt(9)%2;
 			
@@ -31,10 +33,12 @@ public class EmployeeWageComputation {
 					case 0:
 						System.out.println("Employee Is Part Time");
 						daily_emp_wage = WAGE_PER_HOUR * Part_Time_Hour;
+						Total_work_hour = Total_work_hour + Part_Time_Hour;
 						break;
 					case 1:
 						System.out.println("Employee Is Full Time");
 						daily_emp_wage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+						Total_work_hour = Total_work_hour + FULL_DAY_HOUR;
 						break;
 					}
 					
@@ -42,9 +46,12 @@ public class EmployeeWageComputation {
 					System.out.println("Employee Daily Wages :"+daily_emp_wage);
 					break;		
 			}
-					System.out.println("--------------------------");
+			System.out.println("Total Working Hour:"+Total_work_hour);
+			System.out.println("--------------------------");
+			Total_work_days++;
+					
 		}
-					System.out.println("Employee Monthly Wage:" +Monthly_Wage);
+			System.out.println("Employee Monthly Wage:"+Monthly_Wage);
 	}
 
 }     
